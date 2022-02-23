985f814ff365780ffed33ba6a86ade752ec553f6
lemon <tocjo@riseup.net>
2022-02-23 11:38:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="this is hchroku's site!!!!!!!!!!!">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="keywords" content="tocxjo, tocjo, music, idm, neocities">
+      <title>hchroku :D</title>
+      <link rel="icon" href="favicon.ico" type="image/gif"><link/>
+      <link rel="stylesheet" href="styles.css" type="text/css" media="screen"/>
+  </head> 
+  <body style="text-align:center;">
+  <div class="container">
+
+  <!--title-->
+  <div class="title">
+    <h1>hchroku</h1>
+  </div>
+
+  <!--links-->
+<div class="links">
+    <a href="info.php" title="currently under construction">info</a>
+  </div>
+  
+  <div class="pre">
+    <noscript><p>thanks for not using jscript:3</p></noscript>
+    <p>sup, welcome to my website!! i'm hchroku </p>
+  </div>
+
+  <!--info-->
+  <div class="info">
+  </div>
+</div>
+
+  <div class="container">
+
+  <!--couple of links-->
+  <div class="social links">
+  </div>
+
+  <!--88x31's-->
+  <div class="activelinks">
+    <p>
+      <a href="https://kill-9.xyz" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/kill9.png"></a>
+      <a href="https://neocities.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/neocities.gif"></a>
+      <a href="https://kernel.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/linux.png" title="use linux for the sake of god"></a>
+      <a href="https://gnu.org/software/emacs" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/emacs.png" title="emacs is nice."></a>
+      <a href="https://webamp.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/winamp.gif" title="webamp (warning: it has javascript)"></a>
+      <a href='http://www.catb.org/hacker-emblem/'>
+        <img style="border:0;height:31px;" src='http://www.catb.org/hacker-emblem/glider.png'/></a>
+  </div>
+</div>
+
+  <div class="footer">
+    <footer>
+      <p>last edit>>>2022/02/23 @<a href="https://github.com/tocxjo/website.git">source</a></p>
+    </footer>
+  </div>
+</html>
---
+++ info.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="description" content="this is hchroku's site!!!!!!!!!!!">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="keywords" content="tocxjo, tocjo, music, idm, neocities">
+      <title>hchroku :D</title>
+      <link rel="icon" href="favicon.ico" type="image/gif"><link/>
+      <link rel="stylesheet" href="styles.css" type="text/css" media="screen"/>
+  </head> 
+  <body style="text-align:center;">
+
+<div class="container">
+
+  <!--links-->
+<div class="links">
+    <a href="index.php">home</a>
+  </div>
+
+  <div class="pre">
+    <noscript><p>thx for not using jscript :3</p></noscript>
+  </div>
+
+  <!--info-->
+  <div class="info">
+    <h1 class="title">about myself</h1>
+    <p> i'm really passionate about electronics, music, languages and more stuff :DD</p>
+    <hr>
+    </hr>
+    <h1 class="title">about my langs</h1>
+    <p>
+      i speak<a href="https://lukesmith.xyz/c/learning-european-languages-michel-thomas" title="luke smith is so fucking based, check him out">german</a>, spanish,<a href="https://learn.esperanto.com/">esperanto</a>, english, portuguese and now i'm currently learning<a href="https://itazuraneko.neocities.org/">japanese</a>
+    </p>
+  </div>
+</div>
+
+<div class="container">
+
+  <!--couple of links-->
+  <div class="social links">
+  </div>
+
+  <!--88x31's-->
+  <div class="activelinks">
+    <p>
+      <a href="https://kill-9.xyz" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/kill9.png"></a>
+      <a href="https://neocities.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/neocities.gif"></a>
+      <a href="https://kernel.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/linux.png" title="use linux for the sake of god"></a>
+      <a href="https://gnu.org/software/emacs" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/emacs.png" title="emacs is nice."></a>
+      <a href="https://webamp.org" target="_blank">
+        <img style="border:0;width:88px;height:31px;" src="activelinks/winamp.gif" title="webamp (warning: it has javascript)"></a>
+      <a href='http://www.catb.org/hacker-emblem/'>
+        <img style="border:0;height:31px;" src='http://www.catb.org/hacker-emblem/glider.png'/></a>
+  </div>
+</div>
+
+  <div class="footer">
+    <footer>
+      <p>last edit>>>2022/02/23 @<a href="https://github.com/tocxjo/webdite.git">source</a></p>
+    </footer>
+  </div>
+</html>
